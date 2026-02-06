21d8ee8fb032e8e9a48923b3128b600a6a5f280e
Avishi Mehrotra <mehrotraavishi@gmail.com>
2026-02-06 11:58:05 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,15 @@
+class StudentDetails {
+
+    public static void main(String[] args) {
+
+        String name = "Avishi Mehrotra";
+        String sapId = "590014422";
+        String address = "Bareilly, Uttarpradesh";
+        String course = "BTech CSE (AI & ML)";
+
+        System.out.println("Name     : " + name);
+        System.out.println("SAP ID   : " + sapId);
+        System.out.println("Address  : " + address);
+        System.out.println("Course   : " + course);
+    }
+}
